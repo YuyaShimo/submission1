96d085251ff5bd9bb3071e5320ab2cda85cac18a
mei0225 <mei300225@gmail.com>
2020-04-25 19:44:21 +0900

    scriptファイルの編集

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 $(function(){
   $('.faq-list-item').click(function() {
+
     var $answer = $(this).find('.answer');
+
     if($answer.hasClass('open')) {
 
       $answer.removeClass('open');
@@ -8,10 +10,10 @@ $(function(){
       $(this).find('span').text('+');
       
     } else {
-      
+
       $answer.addClass('open'); 
       $answer.slideDown();
       $(this).find('span').text('-');
     }
   });
-  });
+});
